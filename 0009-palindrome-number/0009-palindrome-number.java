db328e21b9603401a6c2e5b0d4ca47d527de604a
BajorskiPatrick <160658460+BajorskiPatrick@users.noreply.github.com>
2025-05-02 16:34:53 +0200

    Time: 6 ms (31.41%), Space: 44 MB (76.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
         String s = Integer.toString(x);
         int left = 0;
         int right = s.length()-1;
@@ -13,5 +17,28 @@ class Solution {
             }
         }
         return true;
+
+        // if (x < 0) {
+        //     return false;
+        // }
+
+        // int i = 1;
+        // int n = 1;
+        // while (i < x) {
+        //     i *= 10;
+        //     n++;
+        // }
+        // i /= 10;
+        // n--;
+
+        // char[] digits = new char[n+1];
+        // while (i >= 1) {
+        //     int tmp = x / i;
+        //     x -= i * tmp;
+        //     i /= 10;
+        //     char[n] = x;
+        //     n--;
+        // }
+
     }
 }
