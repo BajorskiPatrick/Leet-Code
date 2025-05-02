34e8cae59bac4835ce4f2f150987fe6f4a0027fc
BajorskiPatrick <160658460+BajorskiPatrick@users.noreply.github.com>
2025-05-02 17:21:24 +0200

    Time: 7 ms (16.09%), Space: 44 MB (76.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -17,28 +17,5 @@ class Solution {
             }
         }
         return true;
-
-        // if (x < 0) {
-        //     return false;
-        // }
-
-        // int i = 1;
-        // int n = 1;
-        // while (i < x) {
-        //     i *= 10;
-        //     n++;
-        // }
-        // i /= 10;
-        // n--;
-
-        // char[] digits = new char[n+1];
-        // while (i >= 1) {
-        //     int tmp = x / i;
-        //     x -= i * tmp;
-        //     i /= 10;
-        //     char[n] = x;
-        //     n--;
-        // }
-
     }
 }
