bbfe2027e333d5e87ffa265a9db81a8e425b0950
BajorskiPatrick <160658460+BajorskiPatrick@users.noreply.github.com>
2025-05-02 15:49:10 +0200

    Time: 6 ms (31.41%), Space: 43.8 MB (88.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,17 +3,15 @@ class Solution {
         String s = Integer.toString(x);
         int left = 0;
         int right = s.length()-1;
-        boolean res = true;
         while (left < right) {
             if (s.charAt(left) == s.charAt(right)) {
                 left++;
                 right--;
             }
             else {
-                res = false;
-                break;
+                return false;
             }
         }
-        return res;
+        return true;
     }
 }
