7faf2f1d972f5737721821ef6b6ae375b5d3dfc4
BajorskiPatrick <160658460+BajorskiPatrick@users.noreply.github.com>
2025-05-03 20:27:36 +0200

    Time: 15 ms (22.93%), Space: 50.7 MB (22.67%) - LeetHub

---
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int len = tops.length;
+        int min = len;
+        for (int i = 1; i <= 6; i++) {
+            int topCounter = 0;
+            int bottomCounter = 0;
+            int common = 0;
+            for (int j = 0; j < len; j++) {
+                if (tops[j] == bottoms[j] && tops[j] == i) {
+                    common++;
+                    continue;
+                }
+                if (tops[j] == i) {
+                    topCounter++;
+                }
+                if (bottoms[j] == i) {
+                    bottomCounter++;
+                }
+            }
+            if (topCounter + bottomCounter + common == len) {
+                min = Math.min(min, Math.min(topCounter, bottomCounter));
+            }
+        }
+
+        return min < len ? min : -1;
+    }
+}
